d64893866cb42dc6c8be47f1a78ee18b56565fce
markd315 <markd315@gmail.com>
2025-10-25 12:45:39 -0400

    Blockly zoom

--- public/schema-loader.js
+++ public/schema-loader.js
@@ -737,6 +737,15 @@ class S3BlockLoader {
             scrollbars: true,
             trashcan: true,
             theme: themeToUse,
+            zoom: {
+                controls: true,
+                wheel: true,
+                startScale: 1.3,
+                maxScale: 3,
+                minScale: 0.3,
+                scaleSpeed: 1.2,
+                pinch: true
+            }
         });
 
         const startBlock = workspace.newBlock('start');
